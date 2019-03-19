9a2fce9b61f36046f58e21ae9f178edc285fb505
RRuslanIv <r.rusachenka@gmail.com>
2019-03-19 14:24:43 +0300

    exchange currency on coins

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var obj = {
+    "H": 50,
+    "Q": 25,
+    "D": 10,
+    "N": 5,
+    "P": 1
+  }
+  var result = {}
+  for (let key in obj) {
+    if (currency > 10000) {
+        result["error"] = "You are rich, my friend! We don't have so much coins for exchange"
+      } else {
+        var totalDiff = Math.floor(currency / obj[key])
+    	  if (totalDiff >= 1) {
+         result[key] = totalDiff + ''
+         result[key].split('.')[0]
+         currency -= (totalDiff*obj[key])
+    	  } 
+        }
+  }
+  return result
 }
